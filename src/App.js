b5098bb54b79f254bdbd320dc798103b2c86f5e6
Steven Doran <steven.doran@flatironschool.com>
2020-03-04 11:54:41 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import { Component } from "react";
+
 // create your App component here
+
+
+export default class App extends Component {
+  render(){
+
+  }
+
+  componentDidMount(){
+    fetch("http://api.open-notify.org/astros.json")
+  }
+}
